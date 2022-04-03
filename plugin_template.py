08dd72aca61611c216c64f23e4504e489d5aaa7e
Felipe Escoto <felescoto95@hotmail.com>
2022-04-03 16:30:34 -0700

    Initial commit

---
+++ plugin_template.py
@@ -0,0 +1,19 @@
+class Plugin:
+    # The name of the plugin. This string will be displayed in the Plugin menu
+    name = "Template Plugin"
+    # The name of the plugin author
+    author = "Template Author"
+
+    # The HTML that will be loaded when selecting the plugin in the list
+    main_view_html = "<html><body><h2>Hello World</h2></body></html>"
+    # The HTML that will be used to display a widget in the plugin main page
+    tile_view_html = ""
+
+
+    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
+    async def method_1(*args):
+        pass
+
+    # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
+    async def method_2(*args):
+        pass
